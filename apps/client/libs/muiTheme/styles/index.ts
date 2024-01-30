da9ac6856418bca762cd94a307ffb5d3a09b69e5
Kirill Karpov <rin.veber@gmail.com>
2024-01-30 14:57:39 +0400

    add muiTheme

---
+++ apps/client/libs/muiTheme/styles/index.ts
@@ -0,0 +1,37 @@
+export const buttonsStyles = {
+  borderRadius: '8px',
+  hollow: {
+    color: '#000',
+    border: '1px solid #000',
+  },
+
+  default: {
+    color: '#1E1E1E',
+    backgroundColor: '#00A3FF',
+    hover: {
+      backgroundColor: '#90d2f7',
+    },
+  },
+  orange: {
+    color: '#1E1E1E',
+    backgroundColor: '#FF8513',
+    hover: {
+      backgroundColor: '#ffb26b',
+    },
+  },
+
+  link: {
+    color: '#1E1E1E',
+    backgroundColor: '#FF8513',
+  },
+  hover: {
+    opacity: 0.7,
+  },
+};
+
+export const underlineStyles = {
+  borderBottom: '1px solid rgba(0, 0, 0, 0.42)',
+  hover: {
+    borderBottom: '2px solid rgba(0, 0, 0, 0.87)',
+  },
+};
