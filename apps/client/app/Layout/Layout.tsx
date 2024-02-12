b73fe7357d79b45abba1c604cfc0453b6d85a2ea
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2024-02-12 22:58:23 +0500

    fix: created structure

---
+++ apps/client/app/Layout/Layout.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react"
+
+const Layout:FC = () => {
+
+  return (
+    <div></div>
+  )
+}
+
+export default Layout
---
+++ apps/components/UI/button_s/button_s.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {FC} from "react";
+
+const ButtonS:FC = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ButtonS
